821ac5e8f7b662cf189afbea11f02a91fd7b4b96
guzerski <guzerski@gmail.com>
2026-02-04 01:12:50 -0300

    modified:   landing Laura/index.html 	modified:   landing Laura/src/javascript/script.js 	modified:   landing Laura/src/styles/header.css 	modified:   landing Laura/src/styles/home.css 	modified:   landing Laura/src/styles/menu.css 	modified:   landing Laura/src/styles/styles.css
    Implement scroll reveal animations and style adjustments for better UX

--- landing Laura/src/javascript/script.js
+++ landing Laura/src/javascript/script.js
@@ -3,4 +3,48 @@ $(document).ready(function() {
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
     });
+
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    $(window).on('scroll', function() {
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        let activeSectionIndex = 0;
+
+        if(scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        } else{
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
+        }
+
+        sections.each(function(index) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop + section.outerHeight();
+
+            if(scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSectionIndex = index;
+                return false; // Break the loop
+            }
+
+        })
+        navItems.removeClass('active');
+        $(navItems[activeSectionIndex]).addClass('active');
+
+    });
+
+    ScrollReveal().reveal('#cta', {
+        origin: 'top',
+        duration: 1500,
+        distance: '2%'
+    
+    });
+    ScrollReveal().reveal('.tratamento', {
+        origin: 'top',
+        duration: 1500,
+        distance: '2%'
+    });
+
 });
